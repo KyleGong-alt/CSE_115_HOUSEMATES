3827d69cb7f991bfd8251c5c7077cce9d7736a47
javtran <javtran@ucsc.edu>
2022-05-10 15:27:30 -0700

    adding

---
+++ Housemates/Housemates/Home/Chores/AssignMemberCell.swift
@@ -0,0 +1,25 @@
+//
+//  AssignedMemberCell.swift
+//  Housemates
+//
+//  Created by Jackson Tran on 5/8/22.
+//
+
+import UIKit
+
+class AssignMemberCell: UITableViewCell {
+
+    @IBOutlet var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
