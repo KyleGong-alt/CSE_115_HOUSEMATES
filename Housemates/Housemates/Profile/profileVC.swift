eb864cd3f84db44467fd76104103b25db5490731
lavillacrucis <47628317+lavillacrucis@users.noreply.github.com>
2022-05-28 01:09:13 -0700

    Revert "Profilepic shows up in profile page"
    This reverts commit b77434efe5d84530bcfa65ca0841c7504020079a.

--- Housemates/Housemates/Profile/profileVC.swift
+++ Housemates/Housemates/Profile/profileVC.swift
@@ -93,7 +93,7 @@ class profileVC: UIViewController {
         slideUpView.delegate = self
         slideUpView.dataSource = self
         slideUpView.register(SlideUpViewCell.self, forCellReuseIdentifier: "SlideUpViewCell")
-        setProfilePic(currentUser!.email)
+//        setProfilePic()
 //        firstName.text = user.first_name
         // Do any additional setup after loading the view.
     }
@@ -149,8 +149,8 @@ class profileVC: UIViewController {
         
     }
     
-    private func setProfilePic(_ email: String){
-        let urlString = "http://localhost:8080/profilePic?email="+email
+    private func setProfilePic(){
+        let urlString = "http://localhost:8080/profilePic"
         guard let url = URL(string: urlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             if error != nil {
@@ -164,7 +164,6 @@ class profileVC: UIViewController {
             }
 
             DispatchQueue.main.async {
-                print("SWITCHING PROFILE PIC")
                 self.profilePic.image = UIImage(data: data!)
             }
         }.resume()
