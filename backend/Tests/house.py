57e982180402dc46fae02f0338cc01b8a47efdc7
daniel-carrera <dcarrer1@ucsc.edu>
2022-05-31 14:28:19 -0700

    House API unit tests

--- backend/Tests/house.py
+++ backend/Tests/house.py
@@ -12,14 +12,67 @@ class TestHouse:
         self.json_headers = {'Content-Type': 'application/json'}
 
     def test_create_house(self, user_id):
-        create_house_payload = json.dumps({
-            "user_id": str(user_id)
-        })
+        payload = json.dumps({"user_id": str(user_id)})
         url = self.route_dict["create_house"]
-        response = requests.request("POST", url, headers=self.json_headers, data=create_house_payload)
-        print(response.text)
+        response = requests.request("POST", url, headers=self.json_headers, data=payload)
+        # print(response.text)
+
+        response_data = json.loads(response.text)
+        # print(response_data)
+
+        assert(response_data["description"] == "successful query")
+        print("/create_house ... success")
+
+    def test_get_house_members(self, house_code):
+        params = "house_code=" + house_code
+        url = self.route_dict["get_house_members"] + params
+        response = requests.request("GET", url)
+        # print(response.text)
+
+        response_data = json.loads(response.text)
+        # print(response_data)
+
+        assert (response_data["description"] == "successful query")
+        print("/get_house_members ... success")
+
+    def test_join_house(self, user_id, house_code):
+        payload = json.dumps({
+            "user_id": str(user_id),
+            "house_code": house_code
+        })
+        url = self.route_dict["join_house"]
+        response = requests.request("POST", url, headers=self.json_headers, data=payload)
+        # print(response.text)
+
+        response_data = json.loads(response.text)
+        # print(response_data)
+
+        assert (response_data["description"] == "successful join house")
+        print("/join_house ... success")
+
+    def test_leave_house(self, user_id):
+        payload = json.dumps({
+            "user_id": str(user_id),
+        })
+        url = self.route_dict["leave_house"]
+        response = requests.request("POST", url, headers=self.json_headers, data=payload)
+        # print(response.text)
+
+        response_data = json.loads(response.text)
+        # print(response_data)
+
+        assert (response_data["description"] == "successful leave house")
+        print("/leave_house ... success")
 
 
-houseApiTest = TestHouse()
+if __name__ == "__main__":
+    print("-" * 35)
+    print("House API Unit Testing ...\n")
+    # houseApiTest = TestHouse()
+    # houseApiTest.test_create_house(75)
+    # houseApiTest.test_get_house_members("AKZXCOPQ")
+    # houseApiTest.test_join_house(61, "AKZXCOPQ")
+    # houseApiTest.test_leave_house(61)
+    print("\nFinished Running Tests ...")
+    print("-" * 35)
 
-houseApiTest.test_create_house(75)
