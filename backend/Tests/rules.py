065b6a662e3a1eedc998a5e5fc224c73ff81dcee
lavillacrucis <47628317+lavillacrucis@users.noreply.github.com>
2022-05-31 21:54:15 -0700

    Unit test for updateVoteNum finished

--- backend/Tests/rules.py
+++ backend/Tests/rules.py
@@ -10,7 +10,8 @@ class TestRules:
                            "delete_house_rule": self.root_url + "/delete_house_rule",
                            "get_approved_house_rules": self.root_url + "/get_approved_house_rules",
                            "get_not_approved_house_rules": self.root_url + "/get_not_approved_house_rules",
-                           "get_unvoted_house_rules": self.root_url + "/get_unvoted_house_rules"
+                           "get_unvoted_house_rules": self.root_url + "/get_unvoted_house_rules",
+                           "update_house_rule_voted_num": self.root_url + "/update_house_rule_voted_num"
                            }
         self.json_headers = {'Content-Type': 'application/json'}
 
@@ -62,6 +63,38 @@ class TestRules:
         assert (response_data["description"] == "successful query")
         print("/get_house_rules ... success")
 
+    def test_votes(self, user_id = 56, house_code = 'AKZXCOPQ', update_value = -1):
+        sql_getRule = "SELECT id FROM house_rules WHERE title = 'Dont Delete Me'"
+
+        house_rule_json = json.dumps({"title": 'Dont Delete Me',
+                                      "description": 'test',
+                                      "house_code": 'AKZXCOPQ',
+                                      "voted_num": "0",
+                                      "valid": "0"
+                                      })
+        postUrl = self.route_dict["create_house_rules"]
+        postResponse = requests.request("POST", postUrl, headers=self.json_headers, data=house_rule_json)
+
+
+        getUrl = self.route_dict["get_house_rules"]+"?house_code=AKZXCOPQ"
+        getResponse = requests.request("GET", getUrl)
+        response_data = json.loads(postResponse.text)
+
+        #
+        update_vote_data = json.dumps({"user_id": 56,
+                                      "rule_id": 54,
+                                      "update_value": 1
+                                      })
+        voteUrl = self.route_dict["update_house_rule_voted_num"]
+        requests.request("PUT", voteUrl,headers=self.json_headers, data = update_vote_data)
+        voteResponse = requests.request("PUT", voteUrl, headers=self.json_headers, data = update_vote_data)
+
+        vote_response_data = json.loads(voteResponse.text)
+        assert (vote_response_data["description"] == "User has already voted")
+        print("/update_house_rule_voted_num ... success")
+
+
+
 
 
 if __name__ == "__main__":
@@ -75,5 +108,6 @@ if __name__ == "__main__":
     houseApiTest.test_get_approved_house_rules("AKZXCOPQ")
     houseApiTest.test_get_not_approved_house_rules("AKZXCOPQ")
     houseApiTest.test_get_unvoted_house_rules(78, "AKZXCOPQ")
+    houseApiTest.test_votes()
     print("\nFinished Running Tests ...")
     print("-" * 35)
